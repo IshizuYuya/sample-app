43d6400d5ffe3671e1d7c4611dee7bcb99d3b453
IshizuYuya <Ishizu.Yuya@ap.MitsubishiElectric.co.jp>
2019-10-02 10:13:35 +0900

    concourse test commit.

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,7 +7,7 @@ from app.utils import make_message
 
 
 class TestApp(object):
-    """concourse testing."""
+    """concourse testing!!"""
 
     @pytest.mark.parametrize(('name', 'expected'), {
         ('Ishizu', 'Hello Ishizu!'),
