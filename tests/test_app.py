3ef9d7f78eda92175d0ebd6ed781eeb175e437f0
IshizuYuya <Ishizu.Yuya@ap.MitsubishiElectric.co.jp>
2019-10-03 11:37:19 +0900

    concourse test commit.

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,7 +2,6 @@ import pytest
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.dirname(__file__)) + '/..')
-print(sys.path)
 from app.utils import make_message
 
 
@@ -10,8 +9,8 @@ class TestApp(object):
     """concourse testing!!"""
 
     @pytest.mark.parametrize(('name', 'expected'), {
-        ('Ishizu', 'Hello Ishizu!'),
-        ('Yuya', 'Hello Yuya!'),
+        ('Ishizu', 'Hello Ishizu!!'),
+        ('Yuya', 'Hello Yuya!!'),
     })
     def test_app(self, name, expected):
         actual = make_message(name)
