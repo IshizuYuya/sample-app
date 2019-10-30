75ed70f5eb692e71b768ce9f965577d60b9e5f69
IshizuYuya <Ishizu.Yuya@ap.MitsubishiElectric.co.jp>
2019-10-30 10:22:36 +0900

    concourse test commit.

--- ci/scripts/update_manifest.sh
+++ ci/scripts/update_manifest.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/ash
 echo passed1
-tag=`cat sample-app-branch/.git/logs/refs/remotes/origin/HEAD | cut -d ' ' -f 2`
-echo passed2
-git clone sample-app-manifest changed-manifest
-sed -i 's/sample-app:*/sample-app:'${tag}'/g' changed-manifest/manifest.yml
-cd changed-manifest
-git config --global user.name "IshizuYuya"
-git config --global user.email "Ishizu.Yuya@ap.MitsubishiElectric.co.jp"
-git add -A
-git commit -m "update manifest"
+#tag=`cat sample-app-branch/.git/logs/refs/remotes/origin/HEAD | cut -d ' ' -f 2`
+#echo passed2
+#git clone sample-app-manifest changed-manifest
+#sed -i 's/sample-app:*/sample-app:'${tag}'/g' changed-manifest/manifest.yml
+#cd changed-manifest
+#git config --global user.name "IshizuYuya"
+#git config --global user.email "Ishizu.Yuya@ap.MitsubishiElectric.co.jp"
+#git add -A
+#git commit -m "update manifest"
