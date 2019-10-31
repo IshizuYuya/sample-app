98f51e602853bb7a852db35bf3dc25d036238cab
IshizuYuya <Ishizu.Yuya@ap.MitsubishiElectric.co.jp>
2019-10-31 16:10:21 +0900

    concourse test commit.

--- ci/scripts/update_manifest.sh
+++ ci/scripts/update_manifest.sh
@@ -1,7 +1,7 @@
 #!/bin/ash
 tag=`cat sample-app-branch/.git/logs/refs/remotes/origin/HEAD | cut -d ' ' -f 2`
 git clone sample-app-manifest changed-manifest
-sed -i 's/sample-app:*/sample-app:'${tag}'/g' changed-manifest/manifest.yml
+sed -i 's/sample-app:.*/sample-app:'${tag}'/g' changed-manifest/manifest.yml
 cd changed-manifest
 git config --global user.name "IshizuYuya"
 git config --global user.email "Ishizu.Yuya@ap.MitsubishiElectric.co.jp"
